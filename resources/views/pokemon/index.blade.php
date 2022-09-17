94a5529fc324561d7185f642dcac7c36ff674cee
MuhammadHastin <hastinre@gmail.com>
2022-09-17 20:14:03 +0700

    secure

--- resources/views/pokemon/index.blade.php
+++ resources/views/pokemon/index.blade.php
@@ -4,22 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <head>  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></head>
     <title>Pokemon List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&family=Roboto+Flex:opsz,wght@8..144,200&display=swap" rel="stylesheet"> 
     <link  rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-      <link  href="{{ secure_asset('plugins/select2/css/select2.min.css') }}">
-    <link href="{{ secure_asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}" type="text/css">
-    <link href="{{ secure_asset('css/style.css') }}" type="text/css">
+      <link  rel="stylesheet" href="{{ secure_asset('assets/plugins/select2/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}" >
+    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}" >
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="shortcut icon" href="{{ asset('img/pokeball.png') }}" />
-    <script src="{{ asset('assets/plugins/jquery/jquery.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('assets/plugins/select2/js/select2.full.min.js') }}" type="text/javascript"></script>
+    <link rel="shortcut icon" href="{{ secure_asset('img/pokeball.png') }}" />
+    <script src="{{ secure_asset('assets/plugins/jquery/jquery.js') }}" ></script>
+    <script src="{{ secure_asset('assets/plugins/jquery/jquery.min.js') }}" ></script>
+    <script src="{{ secure_asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}" ></script>
+    <script src="{{ secure_asset('assets/plugins/select2/js/select2.full.min.js') }}" ></script>
 </head>
 <script>
     var api = "{{ config('app.api')}}";
@@ -70,7 +69,7 @@
         </div>
     </div>
 @include('pokemon.detail')
-<script src="{{ asset('assets/js/pokemon.js') }}" type="text/javascript"></script>
+<script src="{{ secure_asset('assets/js/pokemon.js') }}"></script>
 
 </body>
 

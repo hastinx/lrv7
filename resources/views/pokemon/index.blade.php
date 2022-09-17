95b07d180ccc3678c41afffa63a6e0aa1e43444b
MuhammadHastin <hastinre@gmail.com>
2022-09-17 16:19:50 +0700

    index public

--- resources/views/pokemon/index.blade.php
+++ resources/views/pokemon/index.blade.php
@@ -10,18 +10,18 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&family=Roboto+Flex:opsz,wght@8..144,200&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-      <link rel="stylesheet" href="./plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="./plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-    <link rel="stylesheet" href="./css/style.css">
+      <link rel="stylesheet" href="/public/plugins/select2/css/select2.min.css">
+    <link rel="stylesheet" href="/public/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css" type="text/css">
+    <link rel="stylesheet" href="/public/css/style.css" type="text/css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="shortcut icon" href="./img/pokeball.png" />
-    <script src="./plugins/jquery/jquery.js"></script>
-    <script src="./plugins/jquery/jquery.min.js"></script>
-    <script src="./plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="./plugins/select2/js/select2.full.min.js"></script>
+    <link rel="shortcut icon" href="/public/img/pokeball.png" />
+    <script src="/public/plugins/jquery/jquery.js"></script>
+    <script src="/public/plugins/jquery/jquery.min.js"></script>
+    <script src="/public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/public/plugins/select2/js/select2.full.min.js"></script>
 </head>
 <script>
-    var api = "{{ config('app.api";
+    var api = "{{ config('app.api')}}";
 </script>
 <body>
     <div class="container-fluid w-50 bg-light">
@@ -69,7 +69,7 @@
         </div>
     </div>
 @include('pokemon.detail')
-<script src="./js/pokemon.js"></script>
+<script src="/public/js/pokemon.js"></script>
 
 </body>
 

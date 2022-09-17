969add0938f19c49dd267fa4616387c75d74b6e5
MuhammadHastin <hastinre@gmail.com>
2022-09-17 21:48:32 +0700

    remake

--- resources/views/pokemon/index.blade.php
+++ resources/views/pokemon/index.blade.php
@@ -10,15 +10,15 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&family=Roboto+Flex:opsz,wght@8..144,200&display=swap" rel="stylesheet"> 
     <link  rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-      <link  rel="stylesheet" href="{{ secure_asset('assets/select2.min.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ secure_asset('assets/select2-bootstrap4.min.css') }}"  type="text/css">
-    <link rel="stylesheet" href="{{ secure_asset('assets/style.css') }}" type="text/css">
+      <link  rel="stylesheet" href="{{ asset('assets/select2.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/select2-bootstrap4.min.css') }}"  type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/style.css') }}" type="text/css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="shortcut icon" href="{{ secure_asset('assets/pokeball.png') }}" type="image/png"/>
-    <script src="{{ secure_asset('assets/jquery.js') }}" type="application/javascript"></script>
-    <script src="{{ secure_asset('assets/jquery.min.js') }}" type="application/javascript"></script>
-    <script src="{{ secure_asset('assets/bootstrap.bundle.min.js') }}" type="application/javascript"></script>
-    <script src="{{ secure_asset('assets/select2.full.min.js') }}" type="application/javascript"></script>
+    <link rel="shortcut icon" href="{{ asset('assets/pokeball.png') }}" type="image/png"/>
+    <script src="{{ asset('assets/jquery.js') }}" type="application/javascript"></script>
+    <script src="{{ asset('assets/jquery.min.js') }}" type="application/javascript"></script>
+    <script src="{{asset('assets/bootstrap.bundle.min.js') }}" type="application/javascript"></script>
+    <script src="{{ asset('assets/select2.full.min.js') }}" type="application/javascript"></script>
 </head>
 <script>
     var api = "{{ config('app.api')}}";
@@ -69,7 +69,7 @@
         </div>
     </div>
 @include('pokemon.detail')
-<script src="{{ secure_asset('assets/pokemon.js') }}" type="application/javascript"></script>
+<script src="{{ asset('assets/pokemon.js') }}" type="application/javascript"></script>
 
 </body>
 

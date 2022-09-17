9b6326316533d18003e8581179e70ad1271a6840
MuhammadHastin <hastinre@gmail.com>
2022-09-17 16:46:20 +0700

    index x-js

--- resources/views/pokemon/index.blade.php
+++ resources/views/pokemon/index.blade.php
@@ -15,10 +15,10 @@
     <link href="/public/css/style.css" type="text/css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="shortcut icon" href="/public/img/pokeball.png" />
-    <script type="text/javascript" src="/public/plugins/jquery/jquery.js"></script>
-    <script type="text/javascript" src="/public/plugins/jquery/jquery.min.js"></script>
-    <script type="text/javascript" src="/public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript" src="/public/plugins/select2/js/select2.full.min.js"></script>
+    <script type="application/x-javascript" src="/public/plugins/jquery/jquery.js"></script>
+    <script type="application/x-javascript" src="/public/plugins/jquery/jquery.min.js"></script>
+    <script type="application/x-javascript" src="/public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script type="application/x-javascript" src="/public/plugins/select2/js/select2.full.min.js"></script>
 </head>
 <script>
     var api = "{{ config('app.api')}}";
@@ -69,7 +69,7 @@
         </div>
     </div>
 @include('pokemon.detail')
-<script type="text/javascript" src="/public/js/pokemon.js"></script>
+<script type="application/x-javascript" src="/public/js/pokemon.js"></script>
 
 </body>
 
